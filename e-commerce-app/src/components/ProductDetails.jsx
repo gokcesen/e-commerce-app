f35f690f479392319427589140e3051b3fb1baf3
gokcesen <sengkce@gmail.com>
2025-10-08 13:06:16 +0200

    style: improve styling of ProductDetails

--- e-commerce-app/src/components/ProductDetails.jsx
+++ e-commerce-app/src/components/ProductDetails.jsx
@@ -12,8 +12,8 @@ const ProductDetails = ({ product }) => {
 
     return(
         <>
-            <div className="max-w-6xl mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-10">
-                <div>
+            <div className="max-w-6xl mx-auto p-6 grid grid-cols-1 md:grid-cols-2 border border-gray-400 rounded shadow gap-10">
+                <div className="bg-gray-200">
                     <img
                         src={product.images[0]}
                         alt={product.title}
@@ -22,32 +22,33 @@ const ProductDetails = ({ product }) => {
                 </div>
                 <div className="flex flex-col justify-start mt-12">
                     <div>
-                        <h1 className="text-4xl font-bold mb-4">{product.title}</h1>
-                        <p className="text-2xl font-semibold text-blue-600 mb-4">
-                        ${product.price}
-                        </p>
-                        <p className="text-gray-700 leading-relaxed text-base mb-4">
+                        <h1 className="text-4xl text-gray-800 font-bold border-b border-gray-400 mb-4 pb-4">{product.title}</h1>
+                        <p className="text-left text-gray-700 leading-relaxed text-base mb-4 pb-6">
                         {product.description}
                         </p>
-                        <QuantityCounter
-                            quantity={cartItem ? cartItem.quantity : 1}
-                            onIncrement={() =>
-                                cartItem
-                                ? updateQuantity(cartItem.id, cartItem.quantity + 1)
-                                : addToCart(product)
-                            }
-                            onDecrement={() =>
-                                cartItem && cartItem.quantity > 1
-                                ? updateQuantity(cartItem.id, cartItem.quantity - 1)
-                                : removeFromCart(cartItem?.id)
-                            }
-                        />
+                        <div className="flex items-center justify-start gap-x-64">
+                            <p className="text-3xl font-semibold text-orange-600">
+                                ${product.price}
+                            </p>
+                            <QuantityCounter
+                                quantity={cartItem ? cartItem.quantity : 1}
+                                onIncrement={() =>
+                                    cartItem
+                                    ? updateQuantity(cartItem.id, cartItem.quantity + 1)
+                                    : addToCart(product)
+                                }
+                                onDecrement={() =>
+                                    cartItem && cartItem.quantity > 1
+                                    ? updateQuantity(cartItem.id, cartItem.quantity - 1)
+                                    : removeFromCart(cartItem?.id)
+                                }
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
         </>
     );
-
 }
 
 export default ProductDetails;
